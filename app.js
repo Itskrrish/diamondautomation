b6dedf8e4c2698f5b395a63502cd765f73813f26
itskrrish <nagpalkrrish32@gmail.com>
2024-04-29 14:18:08 +0530

    diamond commit

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ const PORT = 7001; // Specify the port number
 const server = http.createServer(app);
 app.use(cors());
 app.use(express.static("public"));
+// Middleware to parse JSON and URL-encoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 //    app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 // });
@@ -27,30 +30,13 @@ const io = new Server(server, {
     },
 });
 
-const getallodds = async (req, res) => {
-    try {
-        const allodds = await Odd.find();
-        res.json({ success: true, error: false, odds: allodds });
-    } catch (err) {
-        res.json({ success: false, error: true, fail: err });
-    }
-};
-const gettennisodds = async (req, res) => {
-    try {
-        const allodds = await Tennis.find();
-        res.json({ success: true, error: false, odds: allodds });
-    } catch (err) {
-        res.json({ success: false, error: true, fail: err });
-    }
-};
+
 // Start the server
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   
 
-    // Call openbrowser after the server starts
-    openbrowser();
-    tennisodds();
+  
 });
 
 db.connect((error, success) => {
@@ -63,19 +49,6 @@ db.connect((error, success) => {
     // }
 });
 
-// // mongo connection
-
-// mongoose.connection.on("error", (error) => {
-//     console.error("Error connecting to MongoDB:", error);
-// });
-
-// mongoose.connection.on("open", () => {
-//     console.log("Mongo Db connected");
-// });
-// mongoose.connect(defaultConnectionString);
-// console.log("Mongo Db connected");
-
-//end
 
 app.engine(".hbs", exphbs.engine({ extname: ".hbs", defaultLayout: false }));
 app.set("view engine", ".hbs");
@@ -85,241 +58,248 @@ app.set("view engine", ".hbs");
 //     io.emit("hello", "world", test_object);
 // }
 
-let result_array = [];
-let tennis_odds_array = [];
-async function tennisodds() {
-    //create browser instance
-    // const browser = await puppeteer.launch();
- //const browser = await browser.createIncognitoBrowserContext({ 
-    const browser = await puppeteer.launch({
-      
-    });
 
-//    const context = await browser.createIncognitoBrowserContext({});
-	console.log('BROWSER LAUNCHED');
-    const page = await browser.newPage();
-
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/14.194.102.108 Safari/537.36');
-
-    await page.goto(
-        "https://bxawscf.skyexch.art/exchange/member/index.jsp?eventType=2"
-    );
-
-    console.log('Set screen size')
-    await page.setViewport({ width: 1080, height: 1024 });
-    let btnBackInnerHTML;
-
-    setInterval(async () => {
-        try {
-            await page.waitForSelector("#eventBoard dl", { timeout: 60000 });
-
-
-            tennis_odds_array = await page.$$eval("#eventBoard dl", (dlTags) => {
-                const result = [];
-
-                dlTags.forEach((dlTag) => {
-                    const vsNameElement = dlTag.querySelector("dt a#vsName");
-                    const vsName = dlTag.querySelector("dt a#vsName").innerText;
-                    const vsNameUrl = vsNameElement.getAttribute("href");
-                    const backOdds = [
-                        ...dlTag.querySelectorAll("a#btnBack"),
-                    ].map((aTag) => aTag.innerHTML.replace(/&nbsp;/g, "-"));
-
-                    // Extract lay odds and replace &nbsp; with -
-                    const layOdds = [...dlTag.querySelectorAll("a#btnLay")].map(
-                        (aTag) => aTag.innerHTML.replace(/&nbsp;/g, "-")
-                    );
-
-                    result.push({ vsNameUrl, vsName, backOdds, layOdds });
-                });
-
-                return result;
-            });
-
-            const suspendedurls = tennis_odds_array.map((item) => item.vsNameUrl);
-            try {
-                // Find documents to delete
-                const docsToDelete = await Tennis.find({
-                    vsNameUrl: { $nin: suspendedurls },
-                });
-
-                // Delete documents
-                const deleteResult = await Tennis.deleteMany({
-                    vsNameUrl: { $nin: suspendedurls },
-                });
-            } catch (error) {
-                console.error("Error:", error);
-            }
-
-            // Step 1: Extract all vsNameUrls from the data array
-
-            // console.log('Odds Scraped Successfully":', tennis_odds_array);
-
-            //Update if url exist else create
-            tennis_odds_array.forEach(async (data) => {
-                try {
-                    const { vsNameUrl } = data;
-                    const result = await Tennis.findOneAndUpdate(
-                        { vsNameUrl }, // Search condition
-                        { $set: data }, // Update or Insert document
-                        { upsert: true, new: true } // Options
-                    );
-                    // console.log("Updated or inserted document:", result);
-                } catch (error) {
-                    console.error("Error:", error);
-                }
-            });
-            //end
-
-          
-        } catch (error) {
-            console.log(error);
-        }
-    }, 300);
-
-    return tennis_odds_array;
-}
 
 //Main Function To scrap odds
-async function openbrowser() {
-    //create browser instance
-    // const browser = await puppeteer.launch();
- //const browser = await browser.createIncognitoBrowserContext({ 
-    const browser = await puppeteer.launch({
+// async function openbrowser() {
+//     //create browser instance
+//     // const browser = await puppeteer.launch();
+//  //const browser = await browser.createIncognitoBrowserContext({ 
+//     const browser = await puppeteer.launch({});
+
+// //    const context = await browser.createIncognitoBrowserContext({});
+// 	console.log('BROWSER LAUNCHED');
+//     const page = await browser.newPage();
+
+//     // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/14.194.102.108 Safari/537.36');
+
+//     await page.goto(
+//         "https://bxawscf.skyexch.art/exchange/member/index.jsp?eventType=4"
+//     );
+
+//     console.log('Set screen size')
+//     await page.setViewport({ width: 1080, height: 1024 });
+//     let btnBackInnerHTML;
+
+//     setInterval(async () => {
+//         try {
+//             await page.waitForSelector("#eventBoard dl", { timeout: 60000 });
+
+
+//             result_array = await page.$$eval("#eventBoard dl", (dlTags) => {
+//                 const result = [];
+
+//                 dlTags.forEach((dlTag) => {
+//                     const vsNameElement = dlTag.querySelector("dt a#vsName");
+//                     const vsName = dlTag.querySelector("dt a#vsName").innerText;
+//                     const vsNameUrl = vsNameElement.getAttribute("href");
+//                     const backOdds = [
+//                         ...dlTag.querySelectorAll("a#btnBack"),
+//                     ].map((aTag) => aTag.innerHTML.replace(/&nbsp;/g, "-"));
+
+//                     // Extract lay odds and replace &nbsp; with -
+//                     const layOdds = [...dlTag.querySelectorAll("a#btnLay")].map(
+//                         (aTag) => aTag.innerHTML.replace(/&nbsp;/g, "-")
+//                     );
+
+//                     result.push({ vsNameUrl, vsName, backOdds, layOdds });
+//                 });
+
+//                 return result;
+//             });
+
+//             const suspendedurls = result_array.map((item) => item.vsNameUrl);
+//             try {
+//                 // Find documents to delete
+//                 const docsToDelete = await Odd.find({
+//                     vsNameUrl: { $nin: suspendedurls },
+//                 });
+
+//                 // Delete documents
+//                 const deleteResult = await Odd.deleteMany({
+//                     vsNameUrl: { $nin: suspendedurls },
+//                 });
+//             } catch (error) {
+//                 console.error("Error:", error);
+//             }
+
+//             // Step 1: Extract all vsNameUrls from the data array
+
+//             // console.log('Odds Scraped Successfully":', result_array);
+
+//             //Update if url exist else create
+//             result_array.forEach(async (data) => {
+//                 try {
+//                     const { vsNameUrl } = data;
+//                     const result = await Odd.findOneAndUpdate(
+//                         { vsNameUrl }, // Search condition
+//                         { $set: data }, // Update or Insert document
+//                         { upsert: true, new: true } // Options
+//                     );
+//                     // console.log("Updated or inserted document:", result);
+//                 } catch (error) {
+//                     console.error("Error:", error);
+//                 }
+//             });
+//             //end
+
+//             //emit oddupdate event if mongoDb  is  updated/inserted successfully via socket.io
+
+//             const dbConnection = mongoose.connection;
+//             dbConnection.once("open", async () => {
+//                 try {
+//                     const oddsCollection = dbConnection.collection("odds");
+
+//                     const allOdds = await oddsCollection.find().toArray();
+
+//                     // Emit the oddsUpdate event with all documents
+//                     io.emit("oddsUpdate", allOdds);
+
+//                     // Listen for changes in Odds collection
+//                 } catch (error) {
+//                     console.error("Error:", error);
+//                 }
+//             });
+
+//             //end
+//         } catch (error) {
+//             console.log(error);
+//         }
+//     }, 300);
+
+//     return result_array;
+// }
+
+let OPERATION_TYPE={
+deposit:1,
+withdraw:2
+};
+
+  async function login(req,res) {
+    let success = false;
+    let attempts = 0;
+    while (!success && attempts < 3) {
+    try{  
+    const { username, password,operation_type,amount,transaction_pin,clientsearch } = req.body;
+      const browser = await puppeteer.launch({ headless: false }); // Launch browser in non-headless mode
+      const page = await browser.newPage();
+     // Get the screen width and height
+   
       
-    });
+      // Navigate to the login page
+      await page.goto('https://www.diamondexch99.com/admin/login');
+      // await page.setViewport({ width: 1366, height: 768 });
 
-//    const context = await browser.createIncognitoBrowserContext({});
-	console.log('BROWSER LAUNCHED');
-    const page = await browser.newPage();
-
-    await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/14.194.102.108 Safari/537.36');
-
-    await page.goto(
-        "https://bxawscf.skyexch.art/exchange/member/index.jsp?eventType=4"
-    );
-
-    console.log('Set screen size')
-    await page.setViewport({ width: 1080, height: 1024 });
-    let btnBackInnerHTML;
-
-    setInterval(async () => {
-        try {
-            await page.waitForSelector("#eventBoard dl", { timeout: 60000 });
-
-
-            result_array = await page.$$eval("#eventBoard dl", (dlTags) => {
-                const result = [];
-
-                dlTags.forEach((dlTag) => {
-                    const vsNameElement = dlTag.querySelector("dt a#vsName");
-                    const vsName = dlTag.querySelector("dt a#vsName").innerText;
-                    const vsNameUrl = vsNameElement.getAttribute("href");
-                    const backOdds = [
-                        ...dlTag.querySelectorAll("a#btnBack"),
-                    ].map((aTag) => aTag.innerHTML.replace(/&nbsp;/g, "-"));
-
-                    // Extract lay odds and replace &nbsp; with -
-                    const layOdds = [...dlTag.querySelectorAll("a#btnLay")].map(
-                        (aTag) => aTag.innerHTML.replace(/&nbsp;/g, "-")
-                    );
-
-                    result.push({ vsNameUrl, vsName, backOdds, layOdds });
-                });
-
-                return result;
-            });
-
-            const suspendedurls = result_array.map((item) => item.vsNameUrl);
-            try {
-                // Find documents to delete
-                const docsToDelete = await Odd.find({
-                    vsNameUrl: { $nin: suspendedurls },
-                });
-
-                // Delete documents
-                const deleteResult = await Odd.deleteMany({
-                    vsNameUrl: { $nin: suspendedurls },
-                });
-            } catch (error) {
-                console.error("Error:", error);
-            }
-
-            // Step 1: Extract all vsNameUrls from the data array
-
-            // console.log('Odds Scraped Successfully":', result_array);
-
-            //Update if url exist else create
-            result_array.forEach(async (data) => {
-                try {
-                    const { vsNameUrl } = data;
-                    const result = await Odd.findOneAndUpdate(
-                        { vsNameUrl }, // Search condition
-                        { $set: data }, // Update or Insert document
-                        { upsert: true, new: true } // Options
-                    );
-                    // console.log("Updated or inserted document:", result);
-                } catch (error) {
-                    console.error("Error:", error);
-                }
-            });
-            //end
-
-            //emit oddupdate event if mongoDb  is  updated/inserted successfully via socket.io
-
-            const dbConnection = mongoose.connection;
-            dbConnection.once("open", async () => {
-                try {
-                    const oddsCollection = dbConnection.collection("odds");
-
-                    const allOdds = await oddsCollection.find().toArray();
-
-                    // Emit the oddsUpdate event with all documents
-                    io.emit("oddsUpdate", allOdds);
-
-                    // Listen for changes in Odds collection
-                } catch (error) {
-                    console.error("Error:", error);
-                }
-            });
-
-            //end
-        } catch (error) {
-            console.log(error);
+      async function typeWithDelay(elementHandle, text, delay = 100) {
+          for (const char of text) {
+            await elementHandle.type(char, { delay: delay });
+          }
         }
-    }, 300);
+    
+      // Type username with delay
+    const usernameField = await page.$('#username');
+    await typeWithDelay(usernameField, username);
 
-    return result_array;
-}
+    // Type password with delay
+    const passwordField = await page.$('#Password');
+    await typeWithDelay(passwordField, password);
+    
+      await Promise.all([
+          page.click('.btn-login'), // Click the login button
+          page.waitForNavigation({ timeout: 90000 }), // Wait for navigation to complete
+      
+        ]);
+    
+      // Wait for navigation to complete
+      // await page.waitForNavigation({ timeout: 90000 });
+      // Click on the logo button
+          // Click on the logo button
+      await page.goto('https://www.diamondexch99.com/admin/list-clients/'); // Assuming this element corresponds to the logo you mentioned
+        // Wait for the actions to load
+        await page.waitForSelector('.actions.text-left');
+
+        // Click the first 'a' tag in the actions section (Deposit function)
+      
+
+      // Wait for the list-clients page to load
+      // await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+      
+      console.log('Redirected to list-clients page.');
+    
+
+      
+      await page.waitForSelector('.search input#tags'); // Wait for the input element to be ready
+      const inputElement = await page.$('.search input#tags');
+      await typeWithDelay(inputElement, clientsearch, 100); // Type the client name with a delay
+      await new Promise(resolve => setTimeout(resolve, 10000));
+
+     
+      if (parseInt(operation_type) === OPERATION_TYPE.deposit){
+      console.log('depost');
+      const depositButton = await page.$('.actions.text-left a');
+      await depositButton.click();
+      await page.waitForSelector('#modal-deposit', { visible: true ,timeout: 90000 });
+      // Add a 10-second delay
+    
+
+      // Fill deposit form fields
+      await page.type('#deposite-amount', amount);
+      await page.type('#deposit-remark', 'Automation test');
+      await page.type('#mpassword', transaction_pin);
+      await page.click('#DepositForm button[type="submit"]');
+      }
+      else if (parseInt(operation_type) === OPERATION_TYPE.withdraw)
+      {
+     //withdraw
+     console.log('withdraw');
+      const withdrawtButton = await page.$('.actions.text-left a:nth-child(2)');
+      await withdrawtButton.click();
+      await page.waitForSelector('#modal-withdraw', { visible: true ,timeout: 90000 });
+
+      // Fill withdraw form fields
+      await page.type('#withdraw-amount', amount);
+      await page.type('#withdraw-remark', 'Automation test');
+      await page.type('#withdraw-mpassword', transaction_pin);
+
+      // Submit the form
+      await page.click('#WithdrawForm button[type="submit"]');
+      }
+      else {
+        console.log('nothing');
+        // Handle invalid operation_type
+        return res.status(400).json({ success: false, error: true, message: 'Invalid operation_type' });
+    }
+      return res.status(200).json({success:true,error:false,message:'Operation Successfull'});
+
+      console.log('Clicked on Deposit button.');
+    
+    }
+    catch(e){
+      console.error(e);
+      return res.status(500).json({ success: false, error: true, message: e.message });
+
+    }
+  }
+    
+      // Optionally, you can take a screenshot or perform further actions after login
+      // await page.screenshot({ path: 'loggedin.png' });
+    
+      // Close the browser
+      // await browser.close();
+    }
+    
+  // login();
 
 //end
-app.get("/get-odds", getallodds);
-app.get("/tennis-odds",gettennisodds);
-app.get("/scrape", async (req, res) => {
-    try {
-        const title = await openbrowser();
-
-        res.send(
-            `Scraping completed successfully. betting odds are: "${
-                result_array || "not found"
-            }".`
-        );
-    } catch (error) {
-        // If an error occurs, send a response with the error message
-        res.status(500).send("Error occurred while scraping: " + error.message);
-    }
-});
-app.get("/", (req, res) => {
-    res.render("main");
-});
-// app.get('/', (req, res) => {
-//     res.render("main");
-// });
-app.get('/tennis', (req, res) => {
-    res.render("Tennis");
-});
-app.get('/cricket', (req, res) => {
-    res.render("cricket");
-});
 
 
+
+app.get('/bot', (req, res) => {
+  // Render the "bot" view when a GET request is made to "/bot"
+  res.render('bot');
+});
+app.get('/', (req, res) => {
+  // Render the "bot" view when a GET request is made to "/bot"
+  res.render('home');
+});
+app.post('/bot-start', login);
